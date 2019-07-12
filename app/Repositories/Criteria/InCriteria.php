d092549e09e6174ac4744a613ade23db0203bb85
兴顺 <250915790@qq.com>
2019-07-13 02:00:40 +0800

    查询添加 IN 条件 的,解析

--- app/Repositories/Criteria/InCriteria.php
+++ app/Repositories/Criteria/InCriteria.php
@@ -30,7 +30,7 @@ class InCriteria extends RequestCriteria
             foreach ($fields as $row) {
                 try {
                     list($field, $value) = explode(':', $row);
-                    $searchData[$field] = stripos($value, '-') ? explode('-', $value) : $value;
+                    $searchData[$field] = stripos($value, ',') ? explode(',', $value) : $value;
                 } catch (\Exception $e) {
                     //Surround offset error
                 }
@@ -50,11 +50,10 @@ class InCriteria extends RequestCriteria
 
         $search = parent::parserSearchValue($search);
 
-        if (stripos($search, '-')) {
-            return explode('-', $search);
+        if (stripos($search, ',')) {
+            return explode(',', $search);
         }
 
-        dd($search);
         return $search;
     }
 }
