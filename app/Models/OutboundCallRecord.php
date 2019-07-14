441259b67861ea2c4727e18f1debf45de85f2627
兴顺 <250915790@qq.com>
2019-07-14 21:48:52 +0800

    添加外呼通话记录

---
+++ app/Models/OutboundCallRecord.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Xingshun <250915790@qq.com>
+ * Date: 2019/7/14
+ * Time: 21:48
+ */
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class OutboundCallRecord extends Model
+{
+    protected $table = 'outbound_call_records';
+
+    protected $fillable = [
+
+    ];
+}
